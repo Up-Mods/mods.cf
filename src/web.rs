63aeb02287ead2832950241f24e10c6603f429a9
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-23 15:39:03 +0200

    🚧 redirect root path to Curseforge for now

--- src/web.rs
+++ src/web.rs
@@ -3,6 +3,7 @@ use crate::curseforge::CurseforgeState;
 use axum::Router;
 use axum::routing::get;
 use std::sync::Arc;
+use axum::response::Redirect;
 
 mod files;
 pub mod projects;
@@ -16,6 +17,7 @@ pub fn init_router(eternal_api_token: &str) -> anyhow::Result<Router> {
         curseforge: curseforge::create_state(eternal_api_token)?,
     });
     let router = Router::new()
+        .route("/", get(async || Redirect::to("https://www.curseforge.com")))
         .route("/{project_id}", get(projects::project_by_id))
         .route("/f/{file_id}", get(files::file_by_id))
         .with_state(app_data);
