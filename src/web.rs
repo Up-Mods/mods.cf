0ab88d87f636a1c389e5cb456625d70bd0860fc2
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-23 16:02:23 +0200

    🎨 fix formatting

--- src/web.rs
+++ src/web.rs
@@ -19,8 +19,14 @@ pub fn init_router(eternal_api_token: &str) -> anyhow::Result<Router> {
         curseforge: curseforge::create_state(eternal_api_token)?,
     });
     let router = Router::new()
-        .route("/", get(async || Redirect::to("https://www.curseforge.com")))
-        .route("/health", get(async || HealthResponse::from(StatusCode::OK)))
+        .route(
+            "/",
+            get(async || Redirect::to("https://www.curseforge.com")),
+        )
+        .route(
+            "/health",
+            get(async || HealthResponse::from(StatusCode::OK)),
+        )
         .route("/{project_id}", get(projects::project_by_id))
         .route("/f/{file_id}", get(files::file_by_id))
         .with_state(app_data);
