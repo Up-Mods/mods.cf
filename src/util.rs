3c8666ece95f670a13e2f13f15317c8a169e7f86
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-23 16:01:42 +0200

    ✨ add health check endpoint

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,29 @@
+use axum::Json;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::Serialize;
+
 pub(crate) fn default_true() -> bool {
     true
+}
+
+#[derive(Serialize)]
+pub(crate) struct HealthResponse {
+    pub status: u16,
+    pub message: Option<String>,
+}
+
+impl From<StatusCode> for HealthResponse {
+    fn from(value: StatusCode) -> Self {
+        HealthResponse {
+            status: value.as_u16(),
+            message: value.canonical_reason().map(|it| it.to_string()),
+        }
+    }
+}
+
+impl IntoResponse for HealthResponse {
+    fn into_response(self) -> Response {
+        Json(self).into_response()
+    }
 }
--- src/web.rs
+++ src/web.rs
@@ -1,9 +1,11 @@
 use crate::curseforge;
 use crate::curseforge::CurseforgeState;
+use crate::util::HealthResponse;
 use axum::Router;
+use axum::http::StatusCode;
+use axum::response::Redirect;
 use axum::routing::get;
 use std::sync::Arc;
-use axum::response::Redirect;
 
 mod files;
 pub mod projects;
@@ -18,6 +20,7 @@ pub fn init_router(eternal_api_token: &str) -> anyhow::Result<Router> {
     });
     let router = Router::new()
         .route("/", get(async || Redirect::to("https://www.curseforge.com")))
+        .route("/health", get(async || HealthResponse::from(StatusCode::OK)))
         .route("/{project_id}", get(projects::project_by_id))
         .route("/f/{file_id}", get(files::file_by_id))
         .with_state(app_data);
