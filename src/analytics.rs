ce115c0d02e2a92438a9a04a74e9de2f76cd5ba0
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-10-25 12:38:29 +0200

    🐛 parse scheme for full URL

--- src/analytics.rs
+++ src/analytics.rs
@@ -1,12 +1,14 @@
 use crate::util::{CaptureEventProperties, StatusExt};
-use crate::web::{AppState, get_host};
+use crate::web::AppState;
 use anyhow::{Context, anyhow};
 use axum::extract::{Request, State};
 use axum::http::StatusCode;
 use axum::http::header::USER_AGENT;
 use axum::middleware::Next;
 use axum::response::Response;
+use axum_extra::extract::{Host, Scheme, SchemeMissing};
 use posthog_rs::{Client, ClientOptionsBuilder, Event};
+use regex_macro::regex;
 use std::env;
 use std::sync::Arc;
 
@@ -55,6 +57,8 @@ pub(crate) async fn init(enable: bool) -> anyhow::Result<Analytics> {
 
 pub(crate) async fn capture_analytics(
     State(state): State<Arc<AppState>>,
+    hostname: Option<Host>,
+    scheme: Result<Scheme, SchemeMissing>,
     req: Request,
     next: Next,
 ) -> Result<Response, StatusCode> {
@@ -66,21 +70,32 @@ pub(crate) async fn capture_analytics(
         .get(USER_AGENT)
         .and_then(|h| h.to_str().ok())
         .map(ToString::to_string);
-    let host = get_host(&req, &state.http);
+    let hostname = hostname.map(|h| h.0);
 
-    // URL path
+    // URL
+    let scheme = scheme.ok().map(|s| s.0);
+    let scheme = scheme.as_deref().unwrap_or("http");
     let path = req.uri().clone();
 
-    let response = next.run(req).await;
+    // if no host header or is localhost: try to fall back to frontend url,
+    // otherwise just proceed with whatever we have.
+    let mut url_hostname = hostname.as_deref();
+    if url_hostname.is_none_or(|hostname| regex!(r"^(?:localhost|127\.0\.0\.1)").is_match(hostname))
+    {
+        url_hostname = url_hostname.or(state.http.frontend_url.as_deref())
+    }
 
-    // construct full URL by joining the path to the host
-    // if no host exists we just return the path as is, to still capture that.
-    let full_url = format!("{host}{path}", host = host.as_deref().unwrap_or(""));
+    let full_url = format!(
+        "{scheme}://{host}{path}",
+        host = url_hostname.unwrap_or("localhost")
+    );
+
+    let response = next.run(req).await;
 
     if !IGNORED_PATHS.contains(&path.path()) {
         let event = Event::new_anon("$pageview")
             .with("$current_url", full_url)
-            .with("$host", host)
+            .with("$host", hostname)
             .with("$pathname", path.path())
             .with("status", response.status().as_u16())
             .with("success", response.status().is_success_or_redirect())
--- src/web.rs
+++ src/web.rs
@@ -2,16 +2,11 @@ use crate::analytics::Analytics;
 use crate::curseforge::CurseforgeState;
 use crate::util::HealthResponse;
 use crate::{analytics, curseforge};
-use anyhow::Context;
-use axum::extract::Request;
-use axum::http::header::HOST;
-use axum::http::{HeaderName, StatusCode};
+use axum::http::StatusCode;
 use axum::response::Redirect;
 use axum::routing::get;
 use axum::{Router, middleware};
-use regex_macro::regex;
 use std::env;
-use std::str::FromStr;
 use std::sync::Arc;
 
 mod files;
@@ -24,7 +19,6 @@ pub(crate) struct AppState {
 }
 
 pub(crate) struct HttpConfig {
-    pub host_header_name: HeaderName,
     pub frontend_url: Option<String>,
 }
 
@@ -55,37 +49,9 @@ pub async fn init_router(enable_analytics: bool) -> anyhow::Result<Router> {
 }
 
 fn init_http() -> anyhow::Result<HttpConfig> {
-    let host_header_name = env::var("HOST_HEADER_NAME")
-        .ok()
-        .map(|value| HeaderName::from_str(&value).context("Unable to parse HOST_HEADER_NAME"))
-        .transpose()?
-        .unwrap_or(HOST);
-
     let frontend_url = env::var("FRONTEND_URL").ok();
 
-    Ok(HttpConfig {
-        host_header_name,
-        frontend_url,
-    })
-}
-
-pub(crate) fn get_host(req: &Request, cfg: &HttpConfig) -> Option<String> {
-    let mut host = req
-        .headers()
-        .get(&cfg.host_header_name)
-        .and_then(|h| h.to_str().ok())
-        .map(ToString::to_string);
-
-    // if no host header or is localhost: try to fall back to frontend url,
-    // otherwise just proceed with whatever we have.
-    if host
-        .as_ref()
-        .is_none_or(|value| regex!(r"^https?://(?:localhost|127\.0\.0\.1)").is_match(value))
-    {
-        host = cfg.frontend_url.clone().or(host);
-    }
-
-    host
+    Ok(HttpConfig { frontend_url })
 }
 
 #[cfg(test)]
