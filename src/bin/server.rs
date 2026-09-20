e58eb9cd5fa25892fafa2a2838546936a35324a3
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-09-20 09:24:44 +0200

    🚧 handle shutdown signal

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,6 +2,7 @@ use anyhow::Context;
 use mods_cf::web;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use tokio::net::TcpListener;
+use tokio::signal;
 
 const PORT: u16 = 3000;
 
@@ -16,6 +17,40 @@ async fn main() -> anyhow::Result<()> {
         .with_context(|| format!("Unable to create listener on port {PORT}"))?;
 
     log::info!("Listening on http://localhost:{PORT}");
-    axum::serve(listener, app).await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(wait_for_shutdown_signal())
+        .await?;
+
+    // TODO handle shutdown
+
     Ok(())
+}
+
+async fn wait_for_shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        biased;
+        _ = ctrl_c => {
+            log::info!("Received Ctrl+C, initiating shutdown");
+        }
+        _ = terminate => {
+            log::info!("Received SIGTERM, initiating shutdown");
+        }
+    }
 }
