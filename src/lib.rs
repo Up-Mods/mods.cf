21c9f0c25c3b5a534caacfcd75faba8cccd30314
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-09-24 10:21:31 +0200

    🐛 improve User-Agent string

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,4 @@ mod curseforge;
 mod util;
 pub mod web;
 
-pub const USER_AGENT: &str = "mods.cf/Service (https://github.com/Up-Mods/mods.cf)";
+pub const USER_AGENT: &str = "mods.cf/Service (+https://github.com/Up-Mods/mods.cf)";
